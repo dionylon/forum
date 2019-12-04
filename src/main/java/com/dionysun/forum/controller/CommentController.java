ad2bff48fa5b9277591ae9e10299e4d3b75732fd
dionylon <dionylong@163.com>
2019-12-04 21:55:03 +0800

    entity & add comment

--- src/main/java/com/dionysun/forum/controller/CommentController.java
+++ src/main/java/com/dionysun/forum/controller/CommentController.java
@@ -1,6 +1,9 @@
 package com.dionysun.forum.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.dionysun.forum.common.Result;
+import com.dionysun.forum.common.StatusCode;
 import com.dionysun.forum.entity.Comment;
 import com.dionysun.forum.service.CommentService;
 import org.springframework.beans.factory.ListableBeanFactory;
@@ -20,5 +23,16 @@ public class CommentController {
     @GetMapping("/article/{articleId}/comments")
     public ResponseEntity<List<Comment>> getComments(@PathVariable(name = "articleId") long articleId){
         return ResponseEntity.ok().body(commentService.getCommentsByArticleId(articleId));
+    }
+    @PostMapping("/article/{articleId}/comment")
+    public Result addComment(@PathVariable(name = "articleId")long articleId,
+                             @RequestBody JSONObject body){
+        long userId = body.getLong("userId");
+        String content = body.getString("content");
+        if(commentService.addComment(userId, articleId, content)){
+            return new Result(StatusCode.OK, "评论成功!");
+        } else {
+            return new Result(StatusCode.ERROR, "评论失败");
+        }
     }
 }
